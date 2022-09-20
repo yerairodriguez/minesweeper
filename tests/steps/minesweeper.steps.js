00fbcb4d26611ba51c3b5a0f6dc7d053261505e6
Yerai Rodriguez Mancilla <yerai.rodriguez.7e5@itb.cat>
2022-09-20 10:46:20 +0200

    Scenario: the user opens the app

--- tests/steps/minesweeper.steps.js
+++ tests/steps/minesweeper.steps.js
@@ -2,6 +2,6 @@ const { Given, When, Then } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 const url = 'http://127.0.0.1:5500/';
 
-Given('a user opens the app', async () => {
+Given('the user opens the app', async () => {
 	await page.goto(url);
 });
