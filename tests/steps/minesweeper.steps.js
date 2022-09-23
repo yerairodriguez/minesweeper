a81a97a31e9ff31579ba9014559bd0a04ef585fd
Yerai Rodriguez <yerai.rodriguez.7e5@itb.cat>
2022-09-23 14:03:19 +0200

    step file modified

--- tests/steps/minesweeper.steps.js
+++ tests/steps/minesweeper.steps.js
@@ -5,3 +5,13 @@ const url = 'http://127.0.0.1:5500/';
 Given('the user opens the app', async () => {
 	await page.goto(url);
 });
+
+Then('the number of columns should be: {string}', async function (string) {
+	const board = await page.locator('data-testid=board')
+	expect(board.length).toBe(8);
+  });
+
+Then('the number of rows should be: {string}', async function (string) {
+	const board = await page.locator('data-testid=board')
+	expect(board.length).toBe(8);
+  });
