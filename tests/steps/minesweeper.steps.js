c0bc2096618707988f1c099cde3b9843045edfeb
Yerai Rodriguez Mancilla <yerai.rodriguez.7e5@itb.cat>
2022-10-03 10:49:09 +0200

    Scenario: Validating the dimensions of the board

--- tests/steps/minesweeper.steps.js
+++ tests/steps/minesweeper.steps.js
@@ -2,6 +2,7 @@ const { Given, When, Then } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 const url = 'http://127.0.0.1:5500/';
 
+
 Given('the user opens the app', async () => {
 	await page.goto(url);
 });
@@ -11,7 +12,7 @@ Then('the number of rows in the board should be: {string}', async (string) =>{
 	let numberOfRows = await rows.count();
 	expect(numberOfRows.toString()).toBe(string);
 })
-Then('the number of columns in the board should be: {string}', async (string) =>{
+Then('the number of cells in the board should be: {string}', async (string) =>{
 	const cells = await page.locator('.cell')
 	let numberOfColumns = await cells.count();
 	expect(numberOfColumns.toString()).toBe(string);
