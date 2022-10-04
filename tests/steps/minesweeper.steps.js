804203d236bd294053cca93842d465bc2f2057f5
Yerai Rodriguez Mancilla <yerai.rodriguez.7e5@itb.cat>
2022-10-04 11:06:05 +0200

    Scenario: Validating that all the cells must be unrevealed at the beginning of the game

--- tests/steps/minesweeper.steps.js
+++ tests/steps/minesweeper.steps.js
@@ -6,14 +6,21 @@ const url = 'http://127.0.0.1:5500/';
 Given('the user opens the app', async () => {
 	await page.goto(url);
 });
-
-Then('the number of rows in the board should be: {string}', async (string) =>{
+Then('the number of rows in the board should be: {string}', async (string) => {
 	const rows = await page.locator('data-testid=row')
 	let numberOfRows = await rows.count();
 	expect(numberOfRows.toString()).toBe(string);
-})
-Then('the number of cells in the board should be: {string}', async (string) =>{
+});
+Then('the number of cells in the board should be: {string}', async (string) => {
 	const cells = await page.locator('data-testid=cell')
 	let numberOfColumns = await cells.count();
 	expect(numberOfColumns.toString()).toBe(string);
-})
+});
+Then('cells are all unreveled', async () =>{
+	const cells = await page.locator('data-testid=cell')
+	let numberOfcells = await cells.count();
+	for (let i = 0; i < numberOfcells; i++){
+		let singleCell = await cells.nth(i).isDisabled();
+		expect(singleCell).toBe(false);
+	}	
+});
