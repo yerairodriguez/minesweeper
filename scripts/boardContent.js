0bde0d235c2aeb79749ffaefd79f77ff4476b217
Yerai Rodriguez Mancilla <yerai.rodriguez.7e5@itb.cat>
2022-10-06 12:27:52 +0200

    main refactored

---
+++ scripts/boardContent.js
@@ -0,0 +1,33 @@
+const boardArray = [];
+
+
+export function createBoard(numOfRows, numOfColumns) {
+    let board = document.getElementById('board');
+    for (let r = 1; r <= numOfRows; r++) {
+        let row = createRow(r, numOfColumns);
+        board.appendChild(row);
+    }
+    console.log(boardArray);
+}
+
+function createRow(rowID, numOfColumns) {
+    let row = document.createElement("div")
+    let rowArray = [];
+    row.classList.add("row");
+    row.setAttribute("data-testid", "row");
+    for (let c = 1; c <= numOfColumns; c++) {
+        let cell = createCell(rowID, numOfColumns);
+        row.appendChild(cell);
+        rowArray.push(cell);
+    }
+    boardArray.push(rowArray);
+    return row;
+}
+
+function createCell(rowID, columnID) {
+    let cell = document.createElement("div");
+    cell.id = rowID.toString() + "-" + columnID.toString();
+    cell.classList.add("cell", "unrevealed");
+    cell.setAttribute("data-testid", "cell");
+    return cell;
+}
