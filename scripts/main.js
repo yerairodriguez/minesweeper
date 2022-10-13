1fe02ca37e0d6351661782a7aebbf9eeb4826ed7
Yerai Rodriguez Mancilla <yerai.rodriguez.7e5@itb.cat>
2022-10-13 11:32:09 +0200

    bad coded, revert commit

--- scripts/main.js
+++ scripts/main.js
@@ -58,8 +58,6 @@ function addClickEvent() {
         cells[i].addEventListener("click", function () {
             unrevealCell(this.getAttribute("id"));
             checkCell(this.getAttribute("id"));
-            checkMine(this.getAttribute("id"));
-            checkTile(this.getAttribute("id"));
         });
     }
 }
@@ -117,60 +115,3 @@ function revealMinesWhenGameOver() {
     }
 }
 
-function checkMine(r, c) {
-    if (r < 0 || r >= rows || c < 0 || c >= columns) {
-        return;
-    }
-    if (board[r][c].classList.contains("revealed")) {
-        return;
-    }
-
-    boardArray[r][c].classList.add("revealed");
-    cellsClicked += 1;
-
-    let minesFound = 0;
-
-    minesFound += checkTile(r-1, c-1);      //top left
-    minesFound += checkTile(r-1, c);        //top 
-    minesFound += checkTile(r-1, c+1);      //top right
-
-    minesFound += checkTile(r, c-1);        //left
-    minesFound += checkTile(r, c+1);        //right
-
-    minesFound += checkTile(r+1, c-1);      //bottom left
-    minesFound += checkTile(r+1, c);        //bottom 
-    minesFound += checkTile(r+1, c+1);      //bottom right
-
-    if (minesFound > 0) {
-        boardArray[r][c].innerText = minesFound;
-        boardArray[r][c].classList.add("x" + minesFound.toString());
-    }
-    else {
-        checkMine(r-1, c-1);    //top left
-        checkMine(r-1, c);      //top
-        checkMine(r-1, c+1);    //top right
-
-        checkMine(r, c-1);      //left
-        checkMine(r, c+1);      //right
-
-        checkMine(r+1, c-1);    //bottom left
-        checkMine(r+1, c);      //bottom
-        checkMine(r+1, c+1);    //bottom right
-    }
-
-    if (cellsClicked == rows * columns - minesCount) {
-        document.getElementById("minesCounter").innerText = "Cleared";
-        gameOver = true;
-    }
-
-}
-
-function checkTile(r, c) {
-    if (r < 0 || r >= rows || c < 0 || c >= columns) {
-        return 0;
-    }
-    if (minesLocation.includes(r.toString() + "-" + c.toString())) {
-        return 1;
-    }
-    return 0;
-}
