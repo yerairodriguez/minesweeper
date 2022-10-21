cd24e64d9fd4033cdf6c565376d160071b4d2129
Yerai Rodriguez <yerai.rodriguez.7e5@itb.cat>
2022-10-21 14:14:08 +0200

    Scenario Outline: Validating the default mines left counter value, Scenario: Reset game, return the board and counters to the initial state, Scenario: User game over, cells with mine will be revealed

--- tests/steps/minesweeper.steps.js
+++ tests/steps/minesweeper.steps.js
@@ -2,12 +2,28 @@ const { Given, When, Then } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 const url = 'http://127.0.0.1:5500/';
 
+async function revealCell(cell) {
+	await page.click(`[data-testid="${cell}"]`, { force: true });	
+}
+
+async function tagCell(cell) {
+	await page.click(`[data-testid="${cell}"]`, { button: 'right'});
+}
 
 Given('the user opens the app', async () => {
 	await page.goto(url);
 });
-Given('the user loads the following data:', async (param) => {
+Given('the user loads the following data: {string}', async (param) => {
 	await page.goto(url + "?mockdata=" + param)
+});
+Given('the user reveals cell {string}', async (string) => {
+	await revealCell(string);
+});
+Given('the user tags the cell {string}', async (string) => {
+	await tagCell(string);
+});
+Given('the user resets the game', async () => {
+	await page.click(`[data-testid="resetButton"]`, { force: true });
 });
 Then('the number of rows in the board should be: {string}', async (string) => {
 	const rows = await page.locator('data-testid=row')
@@ -27,4 +43,19 @@ Then('cells are all unreveled', async () =>{
 		expect(singleCell).toBe(false);
 	}	
 });
-
+Then('the left mines counter should be: {string}', async (string) =>{
+	const minesCounter = await page.locator('data-testid=minesCounter').innerText();
+	expect(minesCounter).toBe(string);
+});
+Then('timeCounter display should show the following value: {string}', async (string) =>{
+	const timer = await page.locator('data-testid=timer').innerText();
+	expect(timer).toBe(string);
+});
+Then('cell {string} should be revealed with mine', async (string) =>{
+	const cell = await page.locator(`[data-testid="${string}"]`).innerText();
+	expect(cell).toBe("💣");
+});
+Then('cell {string} should be disabled', async (string) =>{
+	const cell = await page.locator(`[data-testid="${string}"]`)
+	expect(cell).toBeDisabled();
+});
